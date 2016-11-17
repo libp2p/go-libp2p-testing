7b7a0175149a1e6e287cd9d66191ef3b85ec6851
Jeromy <why@ipfs.io>
2016-11-16 17:54:27 -0800

    finish extraction

--- netutil/util.go
+++ netutil/util.go
@@ -4,12 +4,9 @@ import (
 	"context"
 	"testing"
 
-	bhost "github.com/libp2p/go-libp2p/p2p/host/basic"
-	swarm "github.com/libp2p/go-libp2p/p2p/net/swarm"
-
-	metrics "github.com/libp2p/go-libp2p-metrics"
 	inet "github.com/libp2p/go-libp2p-net"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
+	swarm "github.com/libp2p/go-libp2p-swarm"
 	tu "github.com/libp2p/go-testutil"
 	ma "github.com/multiformats/go-multiaddr"
 )
@@ -19,7 +16,7 @@ func GenSwarmNetwork(t *testing.T, ctx context.Context) *swarm.Network {
 	ps := pstore.NewPeerstore()
 	ps.AddPubKey(p.ID, p.PubKey)
 	ps.AddPrivKey(p.ID, p.PrivKey)
-	n, err := swarm.NewNetwork(ctx, []ma.Multiaddr{p.Addr}, p.ID, ps, metrics.NewBandwidthCounter())
+	n, err := swarm.NewNetwork(ctx, []ma.Multiaddr{p.Addr}, p.ID, ps, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -31,11 +28,6 @@ func DivulgeAddresses(a, b inet.Network) {
 	id := a.LocalPeer()
 	addrs := a.Peerstore().Addrs(id)
 	b.Peerstore().AddAddrs(id, addrs, pstore.PermanentAddrTTL)
-}
-
-func GenHostSwarm(t *testing.T, ctx context.Context) *bhost.BasicHost {
-	n := GenSwarmNetwork(t, ctx)
-	return bhost.New(n)
 }
 
 var RandPeerID = tu.RandPeerID
