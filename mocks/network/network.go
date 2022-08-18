32277f8c8083cbcc8639ab9dcd5c807584e0b5b8
Marten Seemann <martenseemann@gmail.com>
2022-08-18 14:39:29 +0300

    remove the resource manager mocks

--- mocks/network/network.go
+++
@@ -1,7 +0,0 @@
-package network
-
-//go:generate sh -c "mockgen -package network -destination mock_resource_manager.go github.com/libp2p/go-libp2p-core/network ResourceManager"
-//go:generate sh -c "mockgen -package network -destination mock_conn_management_scope.go github.com/libp2p/go-libp2p-core/network ConnManagementScope"
-//go:generate sh -c "mockgen -package network -destination mock_stream_management_scope.go github.com/libp2p/go-libp2p-core/network StreamManagementScope"
-//go:generate sh -c "mockgen -package network -destination mock_peer_scope.go github.com/libp2p/go-libp2p-core/network PeerScope"
-//go:generate sh -c "mockgen -package network -destination mock_protocol_scope.go github.com/libp2p/go-libp2p-core/network ProtocolScope"
